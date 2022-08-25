f5fd8ecd77e27867c544557cb0110cf1dcbe3cea
Darcy-dev <58304132+Darcy-dev@users.noreply.github.com>
2022-08-25 09:46:44 -0300

    Update constants.py

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -3,5 +3,5 @@
 URL_ROOT_PROD = 'https://api-pix.gerencianet.com.br'
 CLIENT_ID = 'Client_Id_d8827014b38522f3784e4c6a63937d3cab4dda4f'
 CLIENT_SECRET = 'Client_Secret_dd39faa9d9e4c9d0a091d79d2dc6a3c6c3b7bd71'
-CERTIFICADO = 'src/certificado.pem'
+CERTIFICADO = 'src/prod.pem'
 
